d94825c3dd63d195f35528e3f832add9279b269c
Ricardo Queirós <ricardo.queiros@gmail.com>
2021-11-02 16:12:17 +0000

    Exercise 1 - VueIntro created!

---
+++ vueintro/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Ex1 />
+    <Ex2 />
+    <Ex3 weight="72" />
+    <Ex4 />
+  </div>
+</template>
+
+<script>
+import Ex1 from "./components/Ex1.vue";
+import Ex2 from "./components/Ex2.vue";
+import Ex3 from "./components/Ex3.vue";
+import Ex4 from "./components/Ex4.vue";
+
+export default {
+  name: "App",
+  components: {
+    Ex1,
+    Ex2,
+    Ex3,
+    Ex4,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vueintro/src/components/Ex4.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="intro">
+    <p>Name: {{ productName }}</p>
+    <p v-html="productDesc"></p>
+    <img v-bind:src="productImage" alt="" style="width: 20%" />
+    <p v-if="productPrice < 1000">Price: {{ productPrice }}€</p>
+
+    <p v-if="productAvailability">DISPONÍVEL</p>
+    <p v-else>NÃO DISPONÍVEL</p>
+
+    <button v-on:click="productPrice = 1200">REPOR</button>
+    <button
+      v-bind:disabled="!productAvailability"
+      v-on:click="productAvailability = false"
+    >
+      VENDER
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Ex4",
+  data() {
+    return {
+      productName: "Computador Titan W599",
+      productDesc: "Processador i7<br>Disco SSD 256GB<br> Monitor Samsung 21",
+      productImage:
+        "https://bizon-tech.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/d/s/dsc01648_1.jpg",
+      productAvailability: true,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>

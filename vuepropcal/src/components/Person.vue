79073868a21bdf9aa54955cfd71a3400af441291
Ricardo Queirós <ricardo.queiros@gmail.com>
2021-11-04 22:45:47 +0000

    updated

---
+++ vuepropcal/src/components/Person.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    {{ person }}
+    {{ printDataPerson() }}
+    {{ printDataPersonComputed }}
+    {{ printDataPerson() }}
+    {{ printDataPersonComputed }}
+    <br />
+    {{ getFullNameComputed }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      person: {
+        firstName: "Rui",
+        lastName: "Silva",
+        age: 23,
+      },
+    };
+  },
+  created() {
+    console.log("EVENTO: CREATED");
+  },
+  mounted() {
+    console.log("EVENTO: MOUNTED");
+  },
+  updated() {
+    console.log("EVENTO: UPDATED");
+  },
+  watch: {
+    "person.age"(newValue, oldValue) {
+      console.log(`Idade alterada de ${oldValue} para ${newValue}`);
+    },
+  },
+  methods: {
+    printDataPerson() {
+      console.log(
+        `METHOD--> NOME: ${this.person.firstName} e IDADE: ${this.person.age}`
+      );
+    },
+  },
+  computed: {
+    printDataPersonComputed() {
+      console.log(
+        `COMPUTED--> NOME: ${this.person.firstName} e IDADE: ${this.person.age}`
+      );
+      return true;
+    },
+    getFullNameComputed() {
+      return this.person.firstName + " " + this.person.lastName;
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+</style>

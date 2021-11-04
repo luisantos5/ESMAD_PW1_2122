604e4ef32b6eafbf6ee46efdb226538fb60ffc6f
Ricardo Queirós <ricardo.queiros@gmail.com>
2021-11-04 22:27:01 +0000

    Exercise 3 - VueUpDown created!

--- vueflix/src/App.vue
+++ vueflix/src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <Main />
+    <button @click="increment">+</button>
+    <button @click="decrement" :disabled="num == 0">-</button>
+    <br />
+    <span>{{ num }}</span>
+    <h2 v-if="num < 0">NUMERO INVALIDO</h2>
   </div>
 </template>
 
 <script>
-import Main from "./components/Main.vue";
-
 export default {
   name: "App",
-  components: {
-    Main,
+  data() {
+    return {
+      num: 0,
+    };
+  },
+  created() {
+    if (localStorage.getItem("num")) this.num = localStorage.getItem("num");
+    console.log("Instância criada");
+  },
+  mounted() {
+    console.log("Instância montada");
+  },
+  updated() {
+    localStorage.setItem("num", this.num);
+    console.log("Instância atualizada");
+  },
+  methods: {
+    increment() {
+      this.num++;
+    },
+    decrement() {
+      if (this.num > 0) {
+        this.num--;
+      }
+    },
   },
 };
 </script>

0e296e8ef51d0e54e6421d7866bd78ae6ec071c5
rqueiros <ricardo.queiros@gmail.com>
2021-11-20 13:52:45 +0000

    castles update

--- vuecastles/src/App.vue
+++ vuecastles/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link :to="{ name: 'castles' }">Home</router-link> |
-      <router-link :to="{ name: 'NewCastle' }">Add castle</router-link>
+      <router-link :to="{ name: 'NewCastle' }">Add castlesss</router-link>
     </div>
     <router-view />
   </div>
@@ -18,8 +18,32 @@ export default {
         : [],
     };
   },
+  created() {
+    if (this.castles.length == 0) {
+      this.castles = [
+        {
+          name: "Castelo de Bragança",
+          year: 1910,
+          description:
+            "O Castelo de Bragança localiza-se na freguesia de Santa Maria, no centro histórico da cidade, concelho e distrito de Bragança, em Portugal.",
+          link: "https://pt.wikipedia.org/wiki/Castelo_de_Bragan%C3%A7a",
+          photo:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Castelo_de_Bragan%C3%A7a.jpg/275px-Castelo_de_Bragan%C3%A7a.jpg",
+        },
+        {
+          name: "Castelo de Almourol",
+          year: 1171,
+          description:
+            "O Castelo de Almourol localiza-se na freguesia de Praia do Ribatejo, concelho de Vila Nova da Barquinha, distrito de Santarém, região do Centro (Região das Beiras), em Portugal, embora a sua localização seja frequentemente atribuída a Tancos, visto ser a vila mais perto e onde se vislumbra melhor.",
+          link: "https://pt.wikipedia.org/wiki/Castelo_de_Almourol",
+          photo:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Almourol_034.jpg/300px-Almourol_034.jpg",
+        },
+      ];
+      localStorage.setItem("castles", JSON.stringify(this.castles));
+    }
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
--- vuecastles/src/views/Castle.vue
+++ vuecastles/src/views/Castle.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>Castle details: {{ castle }}</div>
+  <div>
+    <h1>{{ castle.name }} ({{ castle.year }})</h1>
+    <p>{{ castle.description }}</p>
+    <p>
+      Link: <a :href="castle.link" target="_blank">{{ castle.link }}</a>
+    </p>
+    <img :src="castle.photo" alt="" />
+    <br />
+    <a href="/castles">voltar</a>
+  </div>
 </template>
 
 <script>
@@ -7,11 +16,15 @@ export default {
   name: "Castle",
   data() {
     return {
-      castle: this.$route.params.castleName,
+      castle: {},
     };
+  },
+  created() {
+    this.castle = JSON.parse(localStorage.getItem("castles")).find(
+      (castle) => castle.name == this.$route.params.castleName
+    );
   },
 };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
